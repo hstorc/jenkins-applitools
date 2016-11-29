c1ff0c17ba900c3c252f2cc50232cea145b18de9
hstorc <hstorc@grey.com>
2016-11-29 15:54:34 -0500

    first commit

---
+++ simplejenkins.js
@@ -0,0 +1,32 @@
+var webdriver = require('selenium-webdriver');
+
+var driver = new webdriver.Builder()
+    .withCapabilities(webdriver.Capabilities.chrome())
+    .build();
+
+var Eyes = require('eyes.selenium').Eyes;
+var eyes = new Eyes();
+// This is your api key, make sure you use it in all your tests.
+eyes.setApiKey('M2tnJnBwTIj29fU977ntBmvm4oYDi9y7I7VHc361IQ108c110');
+eyes.setForceFullPageScreenshot(true);
+// Start visual testing with browser viewport set to 1024x768.
+// Make sure to use the driver returned through 'then' from this point on.
+eyes.open(driver, 'Applitools', 'taltz'/*a little annoying */)
+    .then(function(driver) {
+        driver.get('https://www.taltz.com/index.html');
+
+        // Visual validation point #1
+       eyes.checkWindow('taltz jenkins');
+
+       // driver.findElement(webdriver.By.css('body > div.modal.modal-hcp-url.is-visible > div > a.button-large.uppercase.continue.wt-taltz-1.js-set-hcp-cookie.wt-enteringAgree')).click();
+       // eyes.checkWindow('after agree');
+        //driver.executeScript("window.scrollTo(0, document.body.scrollHeight)"); 
+        //driver.manage().timeouts().implicitlyWait(1000);       //
+        // Visual validation point #2
+        //eyes.checkWindow('ghost vote after scroll');
+
+        driver.quit();
+
+        // End visual testing. Validate visual correctness.
+        eyes.close();
+    });
