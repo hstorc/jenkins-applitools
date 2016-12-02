1c98ee0d63a4f37987639c6f040e13614308392d
hstorc <hstorc@grey.com>
2016-12-01 22:17:15 -0500

    change

---
+++ simplejenkins.js
@@ -0,0 +1,20 @@
+var webdriver = require('selenium-webdriver');
+
+var driver = new webdriver.Builder()
+    .withCapabilities(webdriver.Capabilities.chrome())
+    .build();
+
+var Eyes = require('eyes.selenium').Eyes;
+var eyes = new Eyes();
+// This is your api key, make sure you use it in all your tests.
+eyes.setApiKey('pf991yE4J9106dZ96FlYXzyzk4bOZXq6Nok6yEcKeAEXWU110');
+eyes.setForceFullPageScreenshot(true);
+eyes.setBatch(process.env.JOB_NAME, process.env.APPLITOOLS_BATCH_ID);
+
+eyes.open(driver, 'Applitools', 'Taltz Homepage'/*a little annoying */)
+    .then(function(driver) {
+        driver.get('https://www.taltz.com/index.html');
+        eyes.checkWindow('taltz jenkins');
+        driver.quit();
+        eyes.close();
+    });
