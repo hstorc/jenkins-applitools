a997ff9b4889990d41ae13f795b94d44cb334a94
hstorc <hstorc@grey.com>
2016-12-02 10:54:51 -0500

    apltiools

--- simplejenkins.js
+++ simplejenkins.js
@@ -9,6 +9,7 @@ var eyes = new Eyes();
 // This is your api key, make sure you use it in all your tests.
 eyes.setApiKey('pf991yE4J9106dZ96FlYXzyzk4bOZXq6Nok6yEcKeAEXWU110');
 eyes.setForceFullPageScreenshot(true);
+console.log('APPLITOOLS_BATCH_ID='+APPLITOOLS_BATCH_ID)
 eyes.setBatch(process.env.JOB_NAME, process.env.APPLITOOLS_BATCH_ID);
 
 eyes.open(driver, 'Applitools', 'Taltz Homepage'/*a little annoying */)
